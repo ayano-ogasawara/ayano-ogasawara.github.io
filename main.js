60488b8fafb302114000bbe8ec09c0b76e16cd91
inakikani <inakikaninaki@gmail.com>
2021-07-30 20:49:33 +0200

    contact form > open mail client for now

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-const x = 'hello'
+const x = 'yo'
 console.log(x)
 window.tocontact = function(){
-    window.location.assign('https://ayan0823.github.io/ux/index.html')
+    window.location.assign('https://ayan0823.github.io/ux/contact.html')
+}
+window.sendemail = function(e){
+    e.preventDefault()
+    const formData = new FormData(e.target)
+    const email = {
+        subject: encodeURIComponent(`UX Inquiry from "${formData.get('contact-email-address')}"`),
+        body: encodeURIComponent(formData.get('contact-message'))
+    }
+    const _a = document.createElement('a')
+    const _clickEvent = new MouseEvent('click')
+    _a.setAttribute('href', `mailto:ayanoveli@gmail.com?subject=${email.subject}&body=${email.body}`)
+    _a.dispatchEvent(_clickEvent)
 }
